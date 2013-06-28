5f2f12347c02dc589aa9908db0cbf1d5b28caece
Cykey <cykey@live.com>
2013-06-28 11:08:56 -0400

    Fixed a memory leak (wasn't really a leak since memory was freed when the program exited, but still).

--- main.m
+++ main.m
@@ -12,7 +12,7 @@
  *     * Redistributions in binary form must reproduce the above copyright
  *       notice, this list of conditions and the following disclaimer in the
  *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the <organization> nor the
+ *     * Neither the name of Cykey nor the
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
  *
@@ -71,6 +71,7 @@ int main(int argc, char **argv, char **envp)
     CFRelease(wiFiAddress);
     CFRelease(bluetoothAddress);
     CFRelease(IMEI);
+    CFRelease(format);
 
     free(buffer);
 
